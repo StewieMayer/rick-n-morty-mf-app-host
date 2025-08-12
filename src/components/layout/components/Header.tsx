ac5d39217bd399058f3a72a67a506aabbf9df045
stewie <stewiemayer@hotmail.com>
2025-08-12 12:08:46 -0600

    feat: se añaden imagenes, se corrijen estilos y se cambia a por link

--- src/components/layout/components/Header.tsx
+++ src/components/layout/components/Header.tsx
@@ -1,16 +1,30 @@
+import LinkedInLogo from "@images/InBug-white.png";
+import GithubLogo from "@images/github-mark-white.png";
+import { Link } from "react-router-dom";
+
 const Header: React.FC = () => {
   return (
-    <header className="flex justify-between items-center px-4 py-5 bg-black text-white">
-      <h1 className="text-2xl shadow-2xl">
-        <a href="/">Rick & Morty Info</a>
+    <header className="flex justify-between items-center px-4 py-3 bg-black text-white">
+      <h1 className="rounded p-2 text-2xl font-bold hover:bg-blue-600 transition-colors duration-300">
+        <Link to="/">Rick & Morty Info</Link>
       </h1>
-      <nav className="flex justify-around">
-        <a href="https://www.linkedin.com/in/antonioamayastc/" target="_blank" rel="noopener noreferrer">
-          <img src="" alt="Linkedin profile" />
-        </a>
-        <a href="https://github.com/StewieMayer?tab=repositories" target="_blank" rel="noopener noreferrer">
-          <img src="" alt="Github repositories" />
-        </a>
+      <nav className="flex gap-1">
+        <Link
+          to="https://www.linkedin.com/in/antonioamayastc/"
+          className="p-2 rounded hover:bg-blue-600 transition-colors duration-300"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={LinkedInLogo} alt="Linkedin profile" className="w-9 h-8" />
+        </Link>
+        <Link
+          to="https://github.com/StewieMayer?tab=repositories"
+          className="p-2 rounded hover:bg-gray-600 transition-colors duration-300"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={GithubLogo} alt="Github repositories" className="w-8 h-8" />
+        </Link>
       </nav>
     </header>
   );
