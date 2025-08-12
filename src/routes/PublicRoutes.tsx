b66ba070e68af576bb3aded70c63aa6bb46de2b5
stewie <stewiemayer@hotmail.com>
2025-08-12 13:58:35 -0600

    feat: se incluye publicRoutes para administrar las rutas

---
+++ src/routes/PublicRoutes.tsx
@@ -0,0 +1,14 @@
+import MfCharacterDetail from "@/remotes/MfCharacterDetail";
+import MfCharacters from "@/remotes/MfCharacters";
+import { Route, Routes } from "react-router-dom";
+
+const PublicRoutes: React.FC = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<MfCharacters />} />
+      <Route path="character/:id/*" element={<MfCharacterDetail />} />
+    </Routes>
+  );
+};
+
+export default PublicRoutes;
