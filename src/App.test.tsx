16624708fc28816a4e9cfe587064b9bbc1b739e2
stewie <stewiemayer@hotmail.com>
2025-08-12 14:09:35 -0600

    chore: se agrega TODO

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from '@/App';
 
+// TODO[SM]: Actualizar test 
 test('renders Host corriendo', () => {
   render(<App />);
   expect(screen.getByText(/Hello Module Federation/i)).toBeInTheDocument();
