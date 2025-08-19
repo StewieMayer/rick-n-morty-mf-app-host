d8db3f5de260120523d550e43fb8413e22566f61
stewie <stewiemayer@hotmail.com>
2025-08-19 13:12:24 -0600

    chore: se elimina alert inecesario

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import Layout from "@components/layout/Layout";
-import { BrowserRouter } from "react-router-dom";
-import PublicRoutes from "@routes/PublicRoutes";
+import Layout from '@components/layout/Layout';
+import { BrowserRouter } from 'react-router-dom';
+import PublicRoutes from '@routes/PublicRoutes';
 
 const App: React.FC = () => {
-
-  alert(process.env.MF_CHARACTER)
-
   return (
     <BrowserRouter>
       <Layout>
