9d599ba3469a8a4a65cfbd72f7d154672b42b75d
stewie <stewiemayer@hotmail.com>
2025-08-24 23:09:25 -0600

    fix: se regresa a la forma basica

--- src/routes/PublicRoutes.tsx
+++ src/routes/PublicRoutes.tsx
@@ -4,8 +4,7 @@ import { Route, Routes, Navigate } from 'react-router-dom';
 const PublicRoutes: React.FC = () => {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/characters/" replace />} />
-      <Route path="/characters/*" element={<MfCharacters />} />
+      <Route path="/*" element={<MfCharacters />} />
     </Routes>
   );
 };
